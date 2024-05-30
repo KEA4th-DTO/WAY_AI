e380d511e75528d47b2f3b190e4e56beaca1ec5c
jmin314 <jmin314@naver.com>
2024-05-30 22:26:05 +0900

    [WAY-AI] print image

--- WAY_AI.py
+++ WAY_AI.py
@@ -28,6 +28,7 @@ def fetch_s3_object(url):
 def CNN(image_stream):
     image_raw = Image.open(image_stream)
     image = image_raw.resize((550, 550))
+    print(image)
     try:
         class CNN(nn.Module):
             def __init__(self):
